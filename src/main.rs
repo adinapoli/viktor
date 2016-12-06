5d1a3d4bae0d840b081101ec5854f1b398472700
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2016-12-06 19:06:46 +0100

    Extracting the images from the page. Yay

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ extern crate hyper;
 
 use hyper::client::Client;
 use std::io::Read;
+use std::iter::FromIterator;
 extern crate select;
 use select::document::Document;
+use select::node::Node;
 use select::predicate::{Predicate, Attr, Name};
 
 static RUNNERS_WORLD_URL: &'static str = "http://www.runnersworld.com/what-to-wear?gender=m&temp=35&conditions=pc&wind=nw&time=dawn&intensity=n&feel=ib";
@@ -20,10 +22,25 @@ fn main() {
 
     for node in document.find(Attr("id", "content")) {
         let pred = Name("table").descendant(Name("table").descendant(Name("td")));
-        let tds = node.find(pred).collect::<Vec<_>>();
-        let images = tds.map(|t| t.find(Name("p")));
-        for td in tds {
-            println!("--> {:?}", td.html())
+        let tds:Vec<_> = node.find(pred).collect();
+
+        /*
+        let myvec = vec![1,2,3,4];
+        let vec2:Vec<_> = myvec.iter().map(|x| x * 2).collect();
+
+        println!("{:?}", myvec);
+        println!("{:?}", vec2);
+         */
+
+        let mut images = Vec::new();
+        for td in &tds {
+            images.extend(td.find(Name("img")));
+        }
+
+        //let images:Vec<_> = tds0.flat_map(|t| t.find(Name("img")).collect::<Vec<_>>()).collect();
+        //let text   = tds.nth(1).map(|t| t.find(Name("p")));
+        for td in images {
+                println!("--> {:?}", td.attr("src").unwrap_or(""))
         }
     }
 }
