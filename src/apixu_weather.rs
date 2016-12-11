77e8e685dd89313f0f0e73a30af52378ff059637
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2016-12-11 17:56:02 +0100

    Correctly get the city from Apixu Weather.

--- src/apixu_weather.rs
+++ src/apixu_weather.rs
@@ -14,12 +14,40 @@ struct ApixuCfg {
     api_key: String,
 }
 
+type City = str;
+
+#[derive(RustcDecodable, Debug, Default)]
+pub struct Location {
+    ///Latitude in decimal degree
+    lat: f32,
+    ///Longitude in decimal degree
+    lon: f32,
+    /// Location name
+    name: String,
+    /// Region or state of the location, if available
+    region: Option<String>,
+    /// Location country
+    country:  String,
+    /// Time zone name
+    tz_id:  String,
+    /// Local date and time in unix time
+    localtime_epoch: u32,
+    /// Local date and time
+    localtime:  String
+}
+
 #[derive(RustcDecodable, Debug, Default)]
 pub struct CurrentWeather {
+    location: Location,
+    current: Current
+}
+
+#[derive(RustcDecodable, Debug, Default)]
+pub struct Current {
     /// Local time when the real time data was updated.
-    last_updated: Option<String>,
+    last_updated: String,
     /// Local time when the real time data was updated in unix time.
-    last_updated_epoch: Option<i32>,
+    last_updated_epoch: i32,
     /// Temperature in celsius
     temp_c: f32,
     /// Temperature in fahrenheit
@@ -32,9 +60,9 @@ pub struct CurrentWeather {
     /// Wind speed in kilometer per hour
     wind_kph: f32,
     /// Wind direction in degrees
-    wind_degree: Option<i32>,
+    wind_degree: i32,
     /// Wind direction as 16 point compass. e.g.: NSW
-    wind_dir: Option<String>,
+    wind_dir: String,
     /// Pressure in millibars
     pressure_mb: f32,
     /// Pressure in inches
@@ -44,14 +72,14 @@ pub struct CurrentWeather {
     /// Precipitation amount in inches
     precip_in: f32,
     /// Humidity as percentage
-    humidity: Option<u8>,
+    humidity: u8,
     /// Cloud cover as percentage
-    cloud: Option<u8>,
+    cloud: u8,
     /// Feels like temperature as celcius
     feelslike_c: f32,
     /// Feels like temperature as fahrenheit
     feelslike_f: f32,
-    is_day: Option<u8>, // 1 = Yes 0 = No
+    is_day: u8, // 1 = Yes 0 = No
 }
 
 #[derive(Debug)]
@@ -83,20 +111,22 @@ impl From<std::io::Error> for ApixuError {
 lazy_static! {
     static ref APIXU_CFG: ApixuCfg = {
         let acfg = ApixuCfg {
-            api_key: env::var("APIXU_API_KEY").unwrap_or(String::from(""))
+            api_key: env::var("APIXU_API_KEY").expect("You need to the env var APIXU_API_KEY to be set.")
         };
         acfg
     };
 }
 
-fn mk_url(uri_path: &str) -> String {
-    String::from(format!("{}{}?key={}", APIXU_URL, uri_path, APIXU_CFG.api_key))
+// Build an Url to be used by Hyper.
+fn mk_url(uri_path: &str, params: Vec<(&str, &str)>) -> String {
+    let param_string: String = params.iter().fold(String::new(), |acc, &x| format!("{}&{}={}", acc, x.0, x.1));
+    String::from(format!("{}{}?key={}{}", APIXU_URL, uri_path, APIXU_CFG.api_key, param_string))
 }
 
 /// Gets the current weather based on Auto IP.
 // TODO: Better error handling.
-pub fn current_weather(client: &hyper::client::Client) -> Result<CurrentWeather, ApixuError> {
-    let url = mk_url("current.json");
+pub fn current_weather(client: &hyper::client::Client, city: Option<&City>) -> Result<CurrentWeather, ApixuError> {
+    let url = mk_url("current.json", vec![("q", city.unwrap_or("auto:ip"))]);
     let mut response = try!(client.get(&url).send());
     if response.status != hyper::status::StatusCode::Ok {
         return Err(ApixuError::InvalidRequest(url, response));
@@ -114,8 +144,11 @@ mod tests {
     #[test]
     fn can_decode_a_current_weather_request() {
         let client = Client::new();
-        match current_weather(&client) {
-            Ok(_) => assert!(true, true),
+        match current_weather(&client, Some("Marsala")) {
+            Ok(cw) => {
+                assert_eq!(cw.location.name, "Marsala");
+                assert_eq!(cw.location.country, "Italy")
+            },
             Err(e) => panic!(format!("{:?}", e)),
         }
     }
